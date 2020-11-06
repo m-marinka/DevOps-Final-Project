c257ea96a29a56c33fb06135f1d91844fcd4dd37
m-marinka <mmakar569@gmail.com>
2020-11-06 00:17:52 -0800

    add an app

---
+++ app/frontend/build/precache-manifest.a982d870b76786d246911add5edeb817.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b1883cf404ad1791908005153ef0c8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d87a6806f4373588163b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "81ddcb598d33745c9fd6",
+    "url": "/static/js/2.324aded8.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.324aded8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d87a6806f4373588163b",
+    "url": "/static/js/main.04475dcc.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  }
+]);
